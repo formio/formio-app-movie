34c05c49e6c3b40e013f583a59c86a74709e7fbe
Travis Tidwell <codertravist@gmail.com>
2015-06-11 22:20:25 -0500

    Patient portal application.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,7 +27,7 @@ angular
       $stateProvider
         .state('home', {
           url: '/?',
-          templateUrl: 'views/main.html'
+          templateUrl: 'views/portal-main.html'
         })
         .state('auth', {
           abstract: true,
@@ -55,6 +55,101 @@ angular
               if (!submission) { return; }
               $rootScope.user = submission;
               $state.go('home');
+            });
+          }]
+        })
+        .state('submissionIndex', {
+          url: '/submission',
+          templateUrl: 'views/submission/index.html',
+          controller: ['$scope', '$state', function ($scope, $state) {
+            $scope.$on('submissionView', function(event, submission) {
+              $state.go('submission.view', {
+                submissionId: submission._id
+              });
+            });
+
+            $scope.$on('submissionEdit', function(event, submission) {
+              $state.go('submission.edit', {
+                submissionId: submission._id
+              });
+            });
+
+            $scope.$on('submissionDelete', function(event, submission) {
+              $state.go('submission.delete', {
+                submissionId: submission._id
+              });
+            });
+          }]
+        })
+        .state('forms', {
+          url: '/forms',
+          templateUrl: 'views/forms.html'
+        })
+        .state('newPatient', {
+          url: '/patient',
+          templateUrl: 'views/patient.html',
+          controller: ['$scope', '$state', function ($scope, $state) {
+            $scope.$on('formSubmission', function(event, submission) {
+              $state.go('submission.view', {submissionId: submission._id});
+            });
+          }]
+        })
+        .state('consent', {
+          url: '/consent',
+          templateUrl: 'views/consent.html',
+          controller: ['$scope', '$state', function ($scope, $state) {
+            $scope.$on('formSubmission', function(event, submission) {
+              $state.go('consentView', {consentId: submission._id});
+            });
+          }]
+        })
+        .state('consentView', {
+          url: '/consent/:consentId',
+          templateUrl: 'views/consent-view.html',
+          controller: ['$scope', '$stateParams', function ($scope, $stateParams) {
+            $scope.consentUrl = $scope.consentForm + '/submission/' + $stateParams.consentId;
+          }]
+        })
+        .state('submission', {
+          abstract: true,
+          url: '/submission/:submissionId',
+          templateUrl: 'views/submission/submission.html',
+          controller: ['$scope', '$stateParams', function ($scope, $stateParams) {
+            $scope.submissionUrl = $scope.patientForm + '/submission/' + $stateParams.submissionId;
+          }]
+        })
+        .state('submission.view', {
+          url: '/',
+          parent: 'submission',
+          templateUrl: 'views/submission/view.html',
+          controller: ['$scope', '$stateParams', 'Formio', function ($scope, $stateParams, Formio) {
+            var loader = new Formio($scope.submissionUrl);
+            $scope.submission = {};
+            $scope.position = {lat: '40.74', lng: '-74.18'};
+            loader.loadSubmission().then(function(submission) {
+              $scope.position.lat = submission.data.address.geometry.location.lat;
+              $scope.position.lng = submission.data.address.geometry.location.lng;
+              $scope.submission = submission;
+            });
+          }]
+        })
+        .state('submission.edit', {
+          url: '/edit',
+          parent: 'submission',
+          templateUrl: 'views/submission/edit.html',
+          controller: ['$scope', '$state', function ($scope, $state) {
+            $scope.$on('formSubmission', function(event, submission) {
+              $state.go('submission.view', {submissionId: submission._id});
+            });
+          }]
+        })
+        .state('submission.delete', {
+          url: '/submission',
+          parent: 'submission',
+          templateUrl: 'views/submission/delete.html',
+          controller: ['$scope', '$state', function ($scope, $state) {
+            $scope.$on('delete', function() {
+              $state.go('submissionIndex');
             });
           }]
         })
@@ -210,31 +305,40 @@ angular
     }
   ])
   .run(['$state', '$rootScope',  'Formio', function($state, $rootScope, Formio) {
-    $rootScope.userLoginForm = 'https://movieapp.form.io/app/api/user/login';
-    $rootScope.userRegisterForm = 'https://movieapp.form.io/app/api/user/register';
-    $rootScope.movieForm = 'https://movieapp.form.io/app/api/movie';
-    $rootScope.directorForm = 'https://movieapp.form.io/app/api/director';
+    $rootScope.userLoginForm = 'http://patientportal.localhost:3000/app/api/patient/login';
+    $rootScope.userRegisterForm = 'http://movieapp.localhost:3000/app/api/user/register';
+    $rootScope.movieForm = 'http://movieapp.localhost:3000/app/api/movie';
+    $rootScope.directorForm = 'http://movieapp.localhost:3000/app/api/director';
+    $rootScope.patientForm = 'http://patientportal.localhost:3000/app/api/patient';
+    $rootScope.patientRegisterForm = 'http://patientportal.localhost:3000/app/api/patient/register';
+    $rootScope.consentForm = 'http://patientportal.localhost:3000/app/api/consent';
+
+    $rootScope.authenticated = !!Formio.getToken();
 
     // Set the current user if it isn't provided.
     if (!$rootScope.user) {
       Formio.currentUser().then(function(user) {
         $rootScope.user = user;
+        $rootScope.authenticated = !!Formio.getToken();
       });
     }
 
     // Ensure they are logged in.
-    $rootScope.$on('$stateChangeStart', function(event, toState) {
+    $rootScope.$on('$stateChangeStart', function() {
       $rootScope.authenticated = !!Formio.getToken();
+      /*
       if (toState.name.substr(0, 4) === 'auth') { return; }
       if(!$rootScope.authenticated) {
         event.preventDefault();
         $state.go('auth.login');
       }
+      */
     });
 
     // Provide a way to logout.
     $rootScope.logout = function() {
       Formio.logout().then(function() {
+        $rootScope.authenticated = false;
         $state.go('auth.login');
       });
     };
