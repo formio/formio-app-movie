68d506be3d2799fe43bef3d6b5dec6ed2c54217a
Travis Tidwell <codertravist@gmail.com>
2015-09-23 22:01:34 -0500

    The movie application.

---
+++ app/config.js
@@ -0,0 +1,4 @@
+angular.module('movieappApp').constant('AppConfig', {
+  appUrl: 'http://{{ path }}.localhost:3000',
+  apiUrl: 'http://api.localhost:3000'
+});
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name movieappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the movieappApp
+ */
+angular.module('movieappApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
