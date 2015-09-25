24525eb3c0d6fe4526e03fe99dcd57586d7bf088
Travis Tidwell <codertravist@gmail.com>
2015-09-25 16:28:07 -0500

    Updating the template.

--- dist/config.template.js
+++ dist/config.template.js
@@ -1,4 +1,4 @@
 angular.module('movieappApp').constant('AppConfig', {
-  appUrl: 'https://{{ path }}.form.io',
-  apiUrl: 'https://api.form.io'
+  appUrl: '{{ protocol }}://{{ path }}.{{ host }}',
+  apiUrl: '{{ protocol }}://api.{{ host }}'
 });
